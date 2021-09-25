b84fced187f28cd89774086811761240a2a2ae7f
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-09-25 12:40:45 +0530

    Disease detection UI

--- client/src/components/Upload/Upload.jsx
+++ client/src/components/Upload/Upload.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import Button from '../CustomButtons/Button'
+import stopImg from '../../assets/img/stop.jpg'
+import processingImg from '../../assets/img/processing.gif';
 
 
 function Upload() {
 
-  const [ image, setImage ] = useState();
+  const [image, setImage ] = useState();
+  const [proc, setProc] = useState(false);
+  const [disease, setDisease] = useState("");
 
-  const uploadData = () => {
+  const uploadData = async () => {
     const uploadData = new FormData();
     uploadData.append('img', image, image.name);
     
@@ -15,7 +19,10 @@ function Upload() {
       body: uploadData,
       
     }).then( 
-        res => console.log(res.json())
+      res => console.log(res.json()),
+      setDisease("Brown Spot"),
+      setProc(false),
+      setImage(null)
     )
     .catch(
         error => console.log(error)
@@ -23,13 +30,24 @@ function Upload() {
   }
 
   return (
-    <>
-    <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
-
-    {image ? <Button color='rose' onClick={() => uploadData()}>Upload</Button>
-    : <Button color='success' disabled={true} onClick={() => uploadData()}>Upload</Button>}
+    <div>
     
-    </>
+      <div style={{"display": "flex","justify-content": "center", "align-items": "center"}}>
+        {proc || image ? <img  alt={"img"} src={processingImg}/> : <img alt={"Img"} src={stopImg}/> }
+      </div>
+      <br />
+
+      <div style={{"display": "flex","justify-content": "center", "align-items": "center"}}>
+        {
+          disease ? <h3>{disease}</h3> : 
+              <div>
+                <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
+                {image ? <Button color='success' onClick={() => uploadData()}>Upload</Button> : <Button color='success' disabled={true} onClick={() => uploadData()}>Upload</Button>}
+              </div>
+        }
+      </div>
+
+    </div>
   );
 }
 
