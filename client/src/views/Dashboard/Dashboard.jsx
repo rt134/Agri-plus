aed9f8e258ec03f798e3cd47da8b4fcd06b41f91
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-09-30 00:25:20 +0530

    Merged with venky and unpdated Dashboard

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,42 +1,20 @@
 import React, { useState, useEffect } from "react";
-// react plugin for creating charts
-// import ChartistGraph from "react-chartist";
-// @material-ui/core
+
 import { makeStyles } from "@material-ui/core/styles";
-// import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import Store from "@material-ui/icons/Store";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
-// import Warning from "@material-ui/icons/Warning";
-// import DateRange from "@material-ui/icons/DateRange";
-// import LocalOffer from "@material-ui/icons/LocalOffer";
-// import Update from "@material-ui/icons/Update";
-// import ArrowUpward from "@material-ui/icons/ArrowUpward";
-// import AccessTime from "@material-ui/icons/AccessTime";
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import PlaceIcon from '@material-ui/icons/Place';
+import CloudIcon from '@material-ui/icons/Cloud';
 import Accessibility from "@material-ui/icons/Accessibility";
-// import BugReport from "@material-ui/icons/BugReport";
-// import Code from "@material-ui/icons/Code";
-// import Cloud from "@material-ui/icons/Cloud";
-// core components
 import GridItem from "../../components/Grid/GridItem.js";
 import GridContainer from "../../components/Grid/GridContainer.js";
 import Table from "../../components/Table/Table.js";
-// import Tasks from "../../components/Tasks/Tasks.js";
-// import CustomTabs from "../../components/CustomTabs/CustomTabs.js";
-// import Danger from "../../components/Typography/Danger.js";
 import Card from "../../components/Card/Card.js";
 import CardHeader from "../../components/Card/CardHeader.js";
 import CardIcon from "../../components/Card/CardIcon.js";
 import CardBody from "../../components/Card/CardBody.js";
 import CardFooter from "../../components/Card/CardFooter.js";
-
-// import { bugs, website, server } from "../../variables/general.js";
-
-// import {
-//   dailySalesChart,
-//   emailsSubscriptionChart,
-//   completedTasksChart,
-// } from "../../variables/charts.js";
 
 import styles from "../../assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
@@ -188,16 +166,28 @@ export default function Dashboard() {
           <Card>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
-                <AttachMoneyIcon />
+                <PlaceIcon />
               </CardIcon>
               <p className={classes.cardCategory}>Place</p>
               <h3 className={classes.cardTitle}>{place}</h3>
             </CardHeader>
             <CardFooter stats>
-              <div className={classes.stats}>
-                {/* <DateRange /> */}
-                {/* Last 24 Hours */}
-              </div>
+              {"India"}
+            </CardFooter>
+          </Card>
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <WatchLaterIcon />
+              </CardIcon>
+              <p className={classes.cardCategory}>Date Time</p>
+              <h4 className={classes.cardTitle}>{dateBuilder(new Date())} </h4>
+            </CardHeader>
+            <CardFooter stats>
+            {time}
             </CardFooter>
           </Card>
         </GridItem>
@@ -206,37 +196,13 @@ export default function Dashboard() {
           <Card>
             <CardHeader color="info" stats icon>
               <CardIcon color="info">
-                <Accessibility />
+                <CloudIcon />
               </CardIcon>
               <p className={classes.cardCategory}>Temperature</p>
-              <h3 className={classes.cardTitle}>{temp}°C</h3>
+              <h3 className={classes.cardTitle}>{temp} °C</h3>
             </CardHeader>
             <CardFooter stats>
-              {/* <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div> */}
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-                <Store />
-              </CardIcon>
-              <p className={classes.cardCategory}>Maximum Temperature</p>
-              <h3 className={classes.cardTitle}>{maxTemp}°C </h3>
-            </CardHeader>
-            <CardFooter stats>
-              {/* <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Get more space
-                </a>
-              </div> */}
+             {temp-2} - {maxTemp+1} °C
             </CardFooter>
           </Card>
         </GridItem>
@@ -245,175 +211,19 @@ export default function Dashboard() {
           <Card>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
-                <AttachMoneyIcon />
+                <WhatshotIcon />
               </CardIcon>
               <p className={classes.cardCategory}>Humidity</p>
               <h3 className={classes.cardTitle}>{humidity}%</h3>
             </CardHeader>
             <CardFooter stats>
-              <div className={classes.stats}>
-                {/* <DateRange /> */}
-                {/* Last 24 Hours */}
-              </div>
+                {description}
             </CardFooter>
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-                <Store />
-              </CardIcon>
-              <p className={classes.cardCategory}>Date and time</p>
-              <h4 className={classes.cardTitle}>{dateBuilder(new Date())} </h4>
-              <h4 className={classes.cardTitle}>{time} </h4>
-            </CardHeader>
-            <CardFooter stats>
-              {/* <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Get more space
-                </a>
-              </div> */}
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <AttachMoneyIcon />
-              </CardIcon>
-              <p className={classes.cardCategory}>Description</p>
-              <h3 className={classes.cardTitle}>{description}</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                {/* <DateRange /> */}
-                {/* Last 24 Hours */}
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
+      
       </GridContainer>
-      {/* <GridContainer>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="success">
-              <ChartistGraph
-                className="ct-chart"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Daily Sales</h4>
-              <p className={classes.cardCategory}>
-                <span className={classes.successText}>
-                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
-                </span>{" "}
-                increase in today sales.
-              </p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> updated 4 minutes ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="warning">
-              <ChartistGraph
-                className="ct-chart"
-                data={emailsSubscriptionChart.data}
-                type="Bar"
-                options={emailsSubscriptionChart.options}
-                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
-                listener={emailsSubscriptionChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Email Subscriptions</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="danger">
-              <ChartistGraph
-                className="ct-chart"
-                data={completedTasksChart.data}
-                type="Line"
-                options={completedTasksChart.options}
-                listener={completedTasksChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Completed Tasks</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer> */}
       <GridContainer>
-        {/* <GridItem xs={12} sm={12} md={6}>
-          <CustomTabs
-            title="Tasks:"
-            headerColor="primary"
-            tabs={[
-              {
-                tabName: "Bugs",
-                tabIcon: BugReport,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0, 3]}
-                    tasksIndexes={[0, 1, 2, 3]}
-                    tasks={bugs}
-                  />
-                ),
-              },
-              {
-                tabName: "Website",
-                tabIcon: Code,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0]}
-                    tasksIndexes={[0, 1]}
-                    tasks={website}
-                  />
-                ),
-              },
-              {
-                tabName: "Server",
-                tabIcon: Cloud,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[1]}
-                    tasksIndexes={[0, 1, 2]}
-                    tasks={server}
-                  />
-                ),
-              },
-            ]}
-          />
-        </GridItem> */}
         <GridItem xs={12} sm={12} md={12}>
           <Card>
             <CardHeader color="success">
