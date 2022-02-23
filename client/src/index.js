1de25c83a68210c401304e480f75430e6246f207
rt134 <rishabh.tripathi134@gmail.com>
2022-02-23 21:33:53 +0530

    Configuring and adding Todo

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
-import Client from "./layouts/Admin.js";
-import Login from "./views/Login/Login";
-import Signup from "./views/Signup/Signup";
+import Login from './views/Login/Login';
+import Client from './layouts/Admin';
+import Signup from './views/Signup/Signup';
 import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
 import "./assets/css/material-dashboard-react.css?v=1.10.0";
@@ -15,10 +15,10 @@ ReactDOM.render(
   <AuthContextProvider>
     <BrowserRouter>
       <Switch>
-        <Route includes path="/client/login" component={Login}></Route>
-        <Route includes path="/client/signup" component={Signup}></Route>
-        <Route path="/client" component={Client} />
-        <Redirect from="/" to="/client/dashboard" />
+            <Route includes path="/client/login" component={Login}></Route>
+            <Route includes path="/client/signup" component={Signup}></Route>
+            <Route path="/client" component={Client} />
+            <Redirect from="/" to="/client/dashboard" />
       </Switch>
     </BrowserRouter>
   </AuthContextProvider>,
